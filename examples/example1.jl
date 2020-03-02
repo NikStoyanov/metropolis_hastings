fe69c83295af9cc6551031486f99bab73fa52330
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-03-02 15:24:32 +0000

    Add example

--- examples/example1.jl
+++ examples/example1.jl
@@ -3,7 +3,7 @@ using metropolis_hastings, Plots
 const α      = 2.35
 const Mmin   = 1.0
 const Mmax   = 100.0
-const N      = 100
+const N      = 10^6
 p = Salpeter(α, 0.0, Mmin, Mmax, N)
 
 const Masses = sample_salpeter(p)
@@ -13,4 +13,6 @@ const D      = sum(LogM)
 const α₀ = 3.0
 const stepsize = 0.005
 
-accepted, αₛ = train(p, α₀, stepsize, 100)
+accepted, αₛ = train(p, α₀, stepsize, 10000)
+
+histogram(αₛ)
