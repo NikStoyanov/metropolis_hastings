57059c467ae6e25d29fd022c55a797f6d672c148
Nikola Stoyanov <nstoyanov.92@gmail.com>
2020-03-02 16:01:06 +0000

    Update example hyperparameters

--- examples/example1.jl
+++ examples/example1.jl
@@ -1,4 +1,4 @@
-using metropolis_hastings, Plots
+using metropolis_hastings, Plots, Statistics
 
 const α      = 2.35
 const Mmin   = 1.0
@@ -11,8 +11,26 @@ const LogM   = log.(Masses)
 const D      = sum(LogM)
 
 const α₀ = 3.0
-const stepsize = 0.005
+const stepsize = 0.01
 
+iter = 10000
 accepted, αₛ = train(p, α₀, stepsize, 10000)
 
-histogram(αₛ)
+# Remove first half of chain and thin
+clean = zeros(ceil(Int, iter/20))
+
+global j = 1
+for i = range(ceil(Int, iter/2), stop=floor(Int, iter) - 1, step=1)
+    if i % 10 == 0
+        clean[j] = αₛ[i]
+        global j = j + 1
+    end
+end
+
+@show (accepted / iter)
+@show clean
+@show mean(clean)
+@show std(clean)
+
+histogram(clean)
+savefig("histogram.svg")
